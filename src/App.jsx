aca276b6ac9bd802225f4ca87a10c35149e4a33d
Wushengyouhan <840835067@qq.com>
2024-12-15 17:12:50 +0800

    214. Using Keys For Resetting Components

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Header />
       <main>
         <ConfigureCounter onSet={handleSetCounter} />
-        <Counter initialCount={chosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
       </main>
     </>
   );
